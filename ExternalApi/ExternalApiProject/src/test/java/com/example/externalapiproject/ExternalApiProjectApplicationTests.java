a37f4bd05f31401ecda66e8c35cd0999fa37afab
DenitsaU <denitsa_d_u@abv.bg>
2022-08-19 13:03:36 +0300

    create project

---
+++ ExternalApi/ExternalApiProject/src/test/java/com/example/externalapiproject/ExternalApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
